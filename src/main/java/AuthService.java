295af9ba90d5d9436e3f24ed9cdbadeefde1f077
BenAychh <ben.tomas.hernandez@gmail.com>
2016-05-21 17:19:02 -0600

    Removed target folder

--- src/main/java/AuthService.java
+++ src/main/java/AuthService.java
@@ -30,26 +30,23 @@ public class AuthService {
 
     private static Route create = new Route() {
         public Object handle(Request request, Response response) throws Exception {
-            System.out.println(request.body());
             String[] userDataArray = request.body().split("&");
             String[] emailPair = userDataArray[0].split("=");
             String[] passwordPair = userDataArray[1].split("=");
             String email = URLDecoder.decode(emailPair[1], "UTF-8");
             String password = URLDecoder.decode(passwordPair[1], "UTF-8");
             password = BCrypt.hashpw(password, BCrypt.gensalt(10));
-            System.out.println(email + " / " + password);
             Connection connection = cpds.getConnection();
             String query = "select email from users where email = ?;";
             PreparedStatement preparedStatement = connection.prepareStatement(query);
             preparedStatement.setString(1, email);
             ResultSet resultSet = preparedStatement.executeQuery();
+            JSONObject object = new JSONObject();
             if (resultSet.next()) {
-                JSONObject object = new JSONObject();
                 object.put("status", 400);
                 object.put("message", "User already exists");
                 response.status(400);
                 response.type("application/json");
-                return object.toString();
             } else {
                 query = "insert into users VALUES (?, ?, ?)";
                 preparedStatement = connection.prepareStatement(query);
@@ -57,14 +54,16 @@ public class AuthService {
                 preparedStatement.setString(2, password);
                 preparedStatement.setBoolean(3, true);
                 preparedStatement.execute();
-                JSONObject object = new JSONObject();
                 object.put("status", 201);
                 object.put("message", "User created");
                 object.put("token", createJWT(email));
                 response.status(201);
                 response.type("application/json");
-                return object.toString();
             }
+            resultSet.close();
+            preparedStatement.close();
+            connection.close();
+            return object.toString();
         }
     };
 
