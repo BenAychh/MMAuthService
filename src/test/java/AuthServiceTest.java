81cc6251624c0e485c71644ae08dc0a57b88fe94
BenAychh <ben.tomas.hernandez@gmail.com>
2016-05-22 16:06:38 -0600

    Writing 404 tests

--- src/test/java/AuthServiceTest.java
+++ src/test/java/AuthServiceTest.java
@@ -18,6 +18,12 @@ import static org.junit.Assert.fail;
  * Created by benhernandez on 5/21/16.
  */
 public class AuthServiceTest {
+    @BeforeClass
+    public static void beforeAll() {
+        String[] args = {};
+        AuthService.main(args);
+    }
+
     @Before
     public void beforeEach() throws SQLException {
         Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost/Users");
@@ -29,7 +35,39 @@ public class AuthServiceTest {
     }
 
     @Test
-    public void testCreate()  throws Exception {
+    public void test404Get() throws Exception {
+        Webb webb = Webb.create();
+        Request request = webb
+                .get("http://localhost:8080/doesnotexist");
+        Response<JSONObject> response = request
+                .asJsonObject();
+        JSONObject result = new JSONObject(response.getErrorBody().toString());
+        System.out.println(result);
+        JSONObject expected = new JSONObject();
+        expected.put("message", "error");
+        expected.put("status", 404);
+        expected.put("requested resource", "/doesnotexist");
+        JSONAssert.assertEquals(expected, result, true);
+    }
+
+    @Test
+    public void test404Post() throws Exception {
+        Webb webb = Webb.create();
+        Request request = webb
+                .get("http://localhost:8080/doesnotexist");
+        Response<JSONObject> response = request
+                .asJsonObject();
+        JSONObject result = new JSONObject(response.getErrorBody().toString());
+        System.out.println(result);
+        JSONObject expected = new JSONObject();
+        expected.put("message", "error");
+        expected.put("status", 404);
+        expected.put("requested resource", "/doesnotexist");
+        JSONAssert.assertEquals(expected, result, true);
+    }
+
+    @Test
+    public void testCreateValidUser()  throws Exception {
         Webb webb = Webb.create();
         Request request = webb
                 .post("http://localhost:8080/create")
@@ -47,6 +85,32 @@ public class AuthServiceTest {
         expected.put("token", "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0QHRlc3QuY29tIn0.egbaJ7yWUvC4mU_C7LNJi24cPNpfx3rlr7woWn9pqsGX6LrGCK2Rf2LaD2cFiJ4AWC93QDMChuCmUM4YtDjzAw");
         JSONAssert.assertEquals(expected, result, true);
         assertEquals(201, response.getStatusCode());
+    }
+
+    @Test
+    public void testCreatedAlreadyExistingUser() throws Exception {
+        Webb webb = Webb.create();
+        Request request = webb
+                .post("http://localhost:8080/create")
+                .param("email", "test@test.com")
+                .param("password", "password");
+        Response<JSONObject> response = request
+                .asJsonObject();
+        JSONObject result = response.getBody();
+        if (result != null) {
+            request = webb
+                    .post("http://localhost:8080/create")
+                    .param("email", "test@test.com")
+                    .param("password", "password");
+            response = request
+                    .asJsonObject();
+            result = new JSONObject(response.getErrorBody().toString());
+            JSONObject expected = new JSONObject();
+            expected.put("message", "User already exists");
+            expected.put("status", 409);
+            JSONAssert.assertEquals(expected, result, true);
+            assertEquals(409, response.getStatusCode());
+        }
     }
 
     @Test
